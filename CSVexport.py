6c56508be36df35ec0a23b772d03e463fe75c06a
Nathan <sysmetryxgosu@gmail.com>
2018-12-02 21:24:45 +0100

    Utils for conversion to csv
    Files are to be placed in the same directory as data directories.
    
    CSVexport.py -> Will extract data from netCDF4 to .csv
    
    cleanCSV.py -> Will remove any row containing a NaN value. 
    
    TODO : Documentation on cleanCSV.py

---
+++ CSVexport.py
@@ -0,0 +1,89 @@
+import os
+import glob
+import netCDF4
+import numpy as np
+import concurrent.futures
+import time
+
+
+def get_file_list(directory):
+    """This function returns a listing of all .nc files in a directory
+
+    Parameters
+    ----------
+    directory : str
+            name or path of the directory
+    """
+
+    if directory is None:
+        print("Directory missing")
+        return None
+    return glob.glob(directory + os.sep + "*.nc")
+
+
+def get_list(file_list, i, variable):
+    """This function returns a list of all data referenced as variable.
+        it transforms the netCDF4 file to a flattened ndarray.
+
+        Parameters
+        ----------
+        file_list : list
+                the list in which to extract the data
+        i : int
+                The file from the list to work on
+        variable : str
+                the variable to extract
+        Returns
+        ----------
+            flattened ndarray.
+        """
+
+    return np.array(netCDF4.Dataset(file_list[i]).variables[variable][:,
+                    :]).flatten().tolist()
+
+
+def export_csv(i):
+    """This function saves all the data in a csv.
+
+            Parameters
+            ----------
+            i : int
+                    The file from the list to work on
+            Returns
+            ----------
+                True if function ended correctly.
+            """
+
+    header = 'lon,lat,sst,412,443,490,555'
+    full_list = [get_file_list('SST'), get_file_list('412'), get_file_list(
+            '443'),
+                 get_file_list('490'), get_file_list('555')]
+    buffer = [get_list(full_list[0], i, 'lon')]
+    buffer += [get_list(full_list[0], i, 'lat')]
+    buffer += [get_list(full_list[0], i, 'SST')]
+    buffer += [get_list(full_list[1], i, 'NRRS412_mean')]
+    buffer += [get_list(full_list[2], i, 'NRRS443_mean')]
+    buffer += [get_list(full_list[3], i, 'NRRS490_mean')]
+    buffer += [get_list(full_list[4], i, 'NRRS555_mean')]
+    buffer = np.array(buffer).T
+    np.savetxt("foo" + str(i) + ".csv", buffer, delimiter=",", header=header)
+    del buffer
+    return True
+
+
+def main():
+    """This is the main function. it is multithreaded using concurrent.futures.
+    Performances drop might be observed if used on a slow hard drive due to
+    high Input/Output.
+            """
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        nb_files = range(184)
+        for i, state in zip(nb_files, executor.map(export_csv, nb_files)):
+                print(str(i) + "   is " + str(state))
+        print("All Done.")
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    main()
+    print("--- %s seconds ---" % (time.time() - start_time))
---
+++ cleanCSV.py
@@ -0,0 +1,41 @@
+import numpy as np
+import concurrent.futures
+import time
+
+
+def load_csv(filename):
+    return np.loadtxt(open(str(filename) + ".csv", "rb"), delimiter=",",
+                      skiprows=1)
+
+
+def save_csv(filename, data):
+    header = "lon,lat,sst,412,443,490,555"
+    np.savetxt(filename + ".csv", data, delimiter=",",
+               header=header)
+
+
+def delete_nan_rows(data):
+    return data[~np.isnan(data).any(axis=1)]
+
+
+def alter_csv(i):
+    filename = "foo" + str(i)
+    data = load_csv(filename)
+    data = delete_nan_rows(data)
+    save_csv(filename, data)
+    del data
+    return True
+
+
+def main():
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        nb_files = range(184)
+        for i, state in zip(nb_files, executor.map(alter_csv, nb_files)):
+                print("Job " + str(i) + "   is done.")
+        print("All Done.")
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    main()
+    print("--- %s seconds ---" % (time.time() - start_time))
